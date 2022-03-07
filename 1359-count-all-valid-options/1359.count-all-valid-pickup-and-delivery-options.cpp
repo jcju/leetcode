ff7f01da874bc04e7402ee204a29008dd9eb66b4
jcju <5397709+jcju@users.noreply.github.com>
2022-03-07 02:05:02 -0500

    1359

---
+++ 1359-count-all-valid-options/1359.count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=1359 lang=cpp
+ *
+ * [1359] Count All Valid Pickup and Delivery Options
+ *
+ * https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/description/
+ *
+ * algorithms
+ * Hard (55.18%)
+ * Likes:    1376
+ * Dislikes: 128
+ * Total Accepted:    47.5K
+ * Total Submissions: 75.2K
+ * Testcase Example:  '1'
+ *
+ * Given n orders, each order consist in pickup and delivery services. 
+ * 
+ * Count all valid pickup/delivery possible sequences such that delivery(i) is
+ * always after of pickup(i). 
+ * 
+ * Since the answer may be too large, return it modulo 10^9 + 7.
+ * 
+ * Example 1:
+ * 
+ * Input: n = 1
+ * Output: 1
+ * Explanation: Unique order (P1, D1), Delivery 1 always is after of Pickup
+ * 1.
+ * 
+ * Example 2:
+ * 
+ * Input: n = 2
+ * Output: 6
+ * Explanation: All possible orders: 
+ * (P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1)
+ * and (P2,D2,P1,D1).
+ * This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery
+ * 2.
+ * 
+ * Example 3:
+ * 
+ * Input: n = 3
+ * Output: 90
+ * 
+ * Constraints:
+ * 1 <= n <= 500
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int MOD = pow(10,9) + 7;
+    int countOrders(int n) {
+        vector<int> record(501);
+        record[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int numSlots = (i - 1) * 2 + 1;
+            long long int numPerm = numSlots * (numSlots + 1) / 2;
+            long long int res = record[i - 1] * numPerm;
+            record[i] = res % MOD;              
+        }
+        return record[n];
+    }
+};
+// @lc code=end
+
---
+++ 1359-count-all-valid-options/README.md
@@ -0,0 +1,10 @@
+### [1359\. Count All Valid Pickup and Delivery Options](https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/)
+
+Difficulty: **Hard**  
+
+Related Topics: [Math](https://leetcode.com/tag/math/), [Dynamic Programming](https://leetcode.com/tag/dynamic-programming/), [Combinatorics](https://leetcode.com/tag/combinatorics/)
+
+Runtime: **0 ms**, faster than **100.00%** of C++ submissions.  
+Memory Usage: **6.1 MB**, less than **30.57%** of C++ submissions.
+
+A DP solution using mathematic permutation method. Time: O(N), Memory O(1).

884b29b8fa2667326c91db8c88898fc606d565d8
jcju <5397709+jcju@users.noreply.github.com>
2022-03-28 19:28:12 -0400

    81

---
+++ 0081-search-in-rotated-sorted-array-ii/81.search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=81 lang=cpp
+ *
+ * [81] Search in Rotated Sorted Array II
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Medium (34.11%)
+ * Likes:    4209
+ * Dislikes: 709
+ * Total Accepted:    410.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+ *
+ * There is an integer array nums sorted in non-decreasing order (not
+ * necessarily with distinct values).
+ * 
+ * Before being passed to your function, nums is rotated at an unknown pivot
+ * index k (0 <= k < nums.length) such that the resulting array is [nums[k],
+ * nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+ * For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and
+ * become [4,5,6,6,7,0,1,2,4,4].
+ * 
+ * Given the array nums after the rotation and an integer target, return true
+ * if target is in nums, or false if it is not in nums.
+ * 
+ * You must decrease the overall operation steps as much as possible.
+ * 
+ * Example 1:
+ * Input: nums = [2,5,6,0,0,1,2], target = 0
+ * Output: true
+ * Example 2:
+ * Input: nums = [2,5,6,0,0,1,2], target = 3
+ * Output: false
+ * 
+ */
+// @lc code=start
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        for (auto num : nums) 
+            if (num == target)
+                return true;
+        return false;
+    }
+};
+// @lc code=end
+
---
+++ 0081-search-in-rotated-sorted-array-ii/README.md
@@ -0,0 +1,16 @@
+### [81\. Search in Rotated Sorted Array II](https://leetcode.com/problems/search-in-rotated-sorted-array-ii/)
+
+Difficulty: **Medium**  
+
+Related Topics: [Array](https://leetcode.com/tag/array/), [Binary Search](https://leetcode.com/tag/binary-search/)
+
+Runtime: **0 ms**, faster than **100.00%** of C++ submissions.  
+Memory Usage: **13.8 MB**, less than **99.50%** of C++ submissions.
+
+Funny fact: a simplest brute-force search method that understandable by kindergarten kids can achieve almost double-100% on this question. Time: O(N); Memory O(1). 
+
+<br/>
+
+
+
+<img src="https://miro.medium.com/max/1400/1*QZ9KsfinTgJT_b1lxul6vw.png" width="1100">
